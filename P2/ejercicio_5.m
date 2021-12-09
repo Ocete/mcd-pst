5f44d0cc4fecfa20d1a88c0ff8a6e947310139b5
Jose <ocetebt@hotmail.com>
2021-12-09 21:52:27 +0100

    Update ejercicio_5.m

--- P2/ejercicio_5.m
+++ P2/ejercicio_5.m
@@ -1,4 +1,4 @@
-L = 1024;
+L = 32;
 N = 1024;
 w_0 = 2*pi/sqrt(31);
 
@@ -7,24 +7,42 @@ w_0 = 2*pi/sqrt(31);
 %plot_exp_shifted(w_0, N, 1024);
 
 % APARTADO B
-%plot_step_shifted(N, 32);
-%plot_step_shifted(N, 64);
-%plot_step_shifted(N, 128);
-%plot_step_shifted(N, 256);
+%plot_rectangular(N, 32);
+%plot_rectangular(N, 64);
+%plot_rectangular(N, 128);
+%plot_rectangular(N, 256);
 
 % APARTADO C
 plot_hanning(N, 32);
+plot_rectangular(N, 32);
+plot_windows_comparative(w_0, N, 32);
 
-function plot_hanning(N, L)
-    x = hanning(L, 'periodic')
+function plot_windows_comparative(w, N, L)
+    j = sqrt(-1);
+    index = [0:L-1];
+    x = exp(j*w*index);
+    x_hanning = hanning(L, 'periodic') .* x;
+
+
+    %hanning_window = hanning(2*L+1, 'periodic');
+    %x_hanning = hanning_window(L+1:2*L+1) .* x;
+
     plot_dtft_amp(x, N, ...
-        strcat('u[n], L=', int2str(L)));
+        strcat('rectangular\_window[n]*exp(j w_0 n), L=', int2str(L)));
+    plot_dtft_amp(x_hanning, N, ...
+        strcat('hanning[n]*exp(j w_0 n), L=', int2str(L)));
 end
 
-function plot_step_shifted(N, L)
+function plot_hanning(N, L)
+    x = hanning(L, 'periodic');
+    plot_dtft_amp(x, N, ...
+        strcat('hanning[n], L=', int2str(L)));
+end
+
+function plot_rectangular(N, L)
     x = [ones(1, L)];
     plot_dtft_amp(x, N, ...
-        strcat('u[n], L=', int2str(L)));
+        strcat('rectangular\_window[n], L=', int2str(L)));
 end
 
 function plot_exp_shifted(w, N, L)
